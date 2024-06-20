fa9a7a8a779e4e313c9cef8d4a86da568bbb8250
Lizbet <lizbetolvera2005@gmail.com>
2024-06-20 12:58:21 -0500

    comenzamos

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Envio de Datos </title>
+</head>
+<body>
+    <form action="welcome.php" method="POST">
+        Nombre:<input type="text" name="name"><br>
+        E-mail:<input type="email" name="email"><br>
+        <input type="submit">
+    
+</body>
+</html>
